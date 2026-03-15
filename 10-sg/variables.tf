e619df365ec73f8f946ea5a469f50c2bae0a1894
meghana1004g-git <meghana1004g@gmail.com>
2026-03-15 11:54:07 +0530

    initial commit

---
+++ 10-sg/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+  default = "roboshop"
+
+}
+
+variable "environment" {
+  default = "dev"
+
+}
+variable "sg_names" {
+  type = list 
+  default = [
+     # Databases
+     "mongodb", "redis", "mysql", "rabbitmq"
+     # Backend
+     "catalogue", "user", "cart", "shipping", "payment",
+     # Backend_ALB
+     "backend_alb",
+     # frontend
+     "frontend",
+     # frontend_alb
+     "frontend_alb",
+     # Bastion
+     "bastion"
+
+  ]
+}
