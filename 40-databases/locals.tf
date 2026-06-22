36de3f09cb6f0da811949a124023ae1be8fa19dc
meghana1004g-git <meghana1004g@gmail.com>
2026-06-22 15:21:05 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -13,5 +13,9 @@ locals {
     mysql_role_name = join("-",[
             for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
          ])
+    mysql_policy_name = join("",[
+            for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
+         ])
+     
 
 }
