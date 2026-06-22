638605d9c1542136e92b6d80bdf1efaddfff6d83
meghana1004g-git <meghana1004g@gmail.com>
2026-06-22 16:01:53 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 component=$1
+environment=$2
 
 dnf install ansible -y
 
@@ -11,4 +12,4 @@ git clone https://github.com/meghana1004g-git/ansible-roboshop-roles.tf.git
 cd ansible-roboshop-roles.tf
 git pull
 
-ansible-playbook -e component=$component roboshop.yaml
+ansible-playbook -e component=$component  -e environment=$environment roboshop.yaml
