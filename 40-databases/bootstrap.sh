fa1651be44ca854434cc1bc181652725b2bd1f59
meghana1004g-git <meghana1004g@gmail.com>
2026-06-22 12:39:26 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -5,10 +5,8 @@ component=$1
 dnf install ansible -y
 
 cd /home/ec2-user
+git clone https://github.com/meghana1004g-git/ansible-roboshop-roles.tf.git
 
-if [ ! -d ansible-roboshop-roles.tf ]; then
-    git clone https://github.com/meghana1004g-git/ansible-roboshop-roles.tf.git
-fi
 
 cd ansible-roboshop-roles.tf
 git pull
