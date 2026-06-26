ba0d49fb4046abbcc4857f0ff72b5bcdd4a7d361
meghana1004g-git <meghana1004g@gmail.com>
2026-06-26 20:22:12 +0530

    terraform

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,18 @@
+variable "project" {
+  default = "roboshop"
+
+}
+
+variable "environment" {
+  default = "dev"
+
+}
+
+variable "zone_id" {
+  default = "Z08708431Z575ORJXVY2S"
+
+}
+
+variable "domain_name" {
+  default = "indurthi.online"
+}
