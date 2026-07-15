c5a06b1b61423384eb9f878e844da98a6edea4a9
meghana1004g-git <meghana1004g@gmail.com>
2026-07-15 15:13:23 +0530

    terraform

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,18 @@
+variable "project" {
+  default = "roboshop"
+
+}
+
+variable "environment" {
+  default = "dev"
+
+}
+
+variable "zone_id" {
+  default = "Z03141781AJ3G4SGDUY0B"
+
+}
+
+variable "domain_name" {
+  default = "indurthi.online"
+}
