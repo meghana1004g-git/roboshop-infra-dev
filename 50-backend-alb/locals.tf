7b9ee546bf99cd716082cd12bf13d6bb45f0b01e
meghana1004g-git <meghana1004g@gmail.com>
2026-07-18 14:20:31 +0530

    terraform

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -1,7 +1,7 @@
 locals {
     backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
     private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-    ami_id = data.aws_ami.joindevops.id
+    #ami_id = data.aws_ami.joindevops.id
     common_tags = {
         Project = var.project
         Environment = var.environment
