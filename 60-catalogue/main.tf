3fe24bed8cb198c60c992fa48c6aa59b2763b6ce
meghana1004g-git <meghana1004g@gmail.com>
2026-07-18 12:16:04 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -122,33 +122,87 @@ tags = merge(
     )
 }
 
-# resource "aws_autoscaling_group" "catalogue" {
-#   name                      = "${var.project}-${var.environment}-catalogue"
-#   max_size                  = 10
-#   min_size                  = 1
-#   health_check_grace_period = 120
-#   health_check_type         = "ELB"
-#   desired_capacity          = 1
-#   force_delete              = false
-#   launch_template {
-#     id      = aws_launch_template.catalogue.id
-#     version = "$Latest"
-#   }
-#   vpc_zone_identifier  = [local.private_subnet_ids]
-#   target_group_arns    = [aws_lb_target_group.catalogue.arn]
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${var.project}-${var.environment}-catalogue"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier  = [local.private_subnet_ids]
+  target_group_arns    = [aws_lb_target_group.catalogue.arn]
   
-#   tag {
-#     key                 = "Name"
-#     value               = "${var.project}-${var.environment}-catalogue"
-#     propagate_at_launch = true
-#   }
-#   timeouts {
-#     delete = "15m"
-#   }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
+  
+  dynamic "tag" {
+    for_each =  merge(
+         {
+           Name = "${var.project}-${var.environment}-catalogue"
+         },
+      local.common_tags
 
-#   tag {
-#     key                 = "lorem"
-#     value               = "ipsum"
-#     propagate_at_launch = false
-#   }
-# } 
+    )
+     content {
+    key                 = each.key
+    value               = each.value
+    propagate_at_launch = true
+     }
+  }
+  # with in 15min autoscalling should be successful
+  timeouts {
+    delete = "15m"
+  }
+}
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${var.project}-${var.environment}-catalogue"
+  policy_type        = "ChangeInCapacity"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 70.0
+  }
+ 
+}
+
+resource "aws_lb_listener_rule" "catalogue" {
+  listener_arn = local.backend_alb_listener_arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.catalogue.arn
+  }
+
+  condition {
+    host_header {
+      values = ["catalogue.backend-alb-${var.environment}.${var.domain_name}"]
+    }
+  }
+}
+
+resource "terraform_data" "catalogue_delete" {
+    triggers_replace = [
+            aws_instance.catalogue.id
+    ]
+    depends_on = [ aws_autoscaling_policy.catalogue ]
+    
+    # it executes in bastion
+    provisioner "local-exec" {
+        command = "aws ec2 terminate-instances ${aws_instance.catalogue.id}"
+    }
+
+}
