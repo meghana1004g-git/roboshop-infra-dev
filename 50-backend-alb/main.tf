3c0d7242a1777fd5a4b90fe250ea8d5fad1fccee
meghana1004g-git <meghana1004g@gmail.com>
2026-07-28 18:17:27 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -1,6 +1,6 @@
 resource "aws_lb" "backend_alb" {
   name               = "${var.project}-${var.environment}" #Roboshop-dev
-  internal           = false
+  internal           = true
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
   subnets            = local.private_subnet_ids
