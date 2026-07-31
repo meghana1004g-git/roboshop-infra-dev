83901fd7890803967d583f7e46e60188168ed171
meghana1004g-git <meghana1004g@gmail.com>
2026-07-31 19:55:52 +0530

    terraform

---
+++ 90-components/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.33.0" # Terraform aws provider version
+    }
+  }
+  
+  backend "s3" {
+    bucket         = "remote-state-s3bucket-dev" #replace with your bucket name
+    key            = "roboshop-dev-components"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+}
